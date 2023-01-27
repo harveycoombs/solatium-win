c314e752a3d79af60607bc93ed75c460cd490a4f
harveycoombs <harveycoombs23@outlook.com>
2023-01-27 22:22:14 +0000

    Add project files.

---
+++ solatium.cpp
@@ -0,0 +1,23 @@
+/*
+------------------------------------------------
+
+	Solatium for Windows ~ ./solatium.cpp
+	Written & maintained by Harvey S. Coombs
+	Last edited Jan 28, 2023
+
+------------------------------------------------
+*/
+#include <iostream>
+#include <stdlib.h>
+#include <filesystem>
+
+namespace filesys = std::filesystem;
+
+struct backup_detail {
+	int index;
+	std::string path;
+};
+
+int main() {
+	//to-do
+}
